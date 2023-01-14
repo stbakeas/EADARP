4a8380647b2cac0b015bbebd22bc502688f65cab
p17078@unipi.gr <p17078@unipi.gr>
2023-01-14 09:54:30 +0200

    added minor changes to Request and EAV class

--- EADARP/EAV.h
+++ EADARP/EAV.h
@@ -11,4 +11,5 @@ public:
 	EAV(int id, int capacity, double battery, double start_time, double end_time, double battery_return_percentage);
 	~EAV() {};
 	bool operator==(const EAV& a) const;
+	bool operator<(const EAV& a) const;
 };
