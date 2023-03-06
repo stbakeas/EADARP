9ca57b2d2937043011b2dec2d629ea049f6c7e76
p17078@unipi.gr <p17078@unipi.gr>
2023-03-06 15:29:59 +0200

    removed direction property from Request class

--- EADARP/Request.cpp
+++ EADARP/Request.cpp
@@ -1,12 +1,7 @@
 #include "Request.h"
 #include "Instance.h"
 
-Request::Request(Node* origin,Node* destination,Direction direction) {
+Request::Request(Node* origin,Node* destination) {
 	this->origin = origin;
 	this->destination = destination;
-	this->direction = direction;
-}
-
-Node* Request::getCriticalVertex() {
-	return direction == Request::Direction::INBOUND ? origin : destination;
 }
--- EADARP/Request.h
+++ EADARP/Request.h
@@ -6,13 +6,10 @@
 
 class Request {
 public:
-	enum class Direction{ INBOUND,OUTBOUND };
-	Direction direction;
 	Node* origin;
 	Node* destination;
 	std::unordered_set<int> forbidden_vehicles;
 	Request() {};
-	Request(Node* origin, Node* destination,Direction direction);
-	Node* getCriticalVertex();
+	Request(Node* origin, Node* destination);
 	~Request() {};
 };
