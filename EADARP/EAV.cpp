f9864af3b3afd35307cec6b0d778a4e5097b41b0
p17078@unipi.gr <p17078@unipi.gr>
2023-03-20 13:27:50 +0200

    added vehicle initial battery level (previously assumed the vehicle started full)

--- EADARP/EAV.cpp
+++ EADARP/EAV.cpp
@@ -1,12 +1,14 @@
 #include "EAV.h"
-EAV::EAV(int id, int capacity, double battery, double start_time, double end_time, double battery_return_percentage) {
+
+EAV::EAV(int id, int capacity, double battery, double start_time, double end_time, double initial_battery, double battery_return_percentage) {
 	this->id = id;
 	this->capacity = capacity;
-	this->battery = battery;
+	this->total_battery = battery;
 	this->start_time = start_time;
 	this->end_time = end_time;
-	this->battery_return_percentage=battery_return_percentage;
-	this->acquisition_cost = capacity + battery;
+	this->battery_return_percentage = battery_return_percentage;
+	this->initial_battery = initial_battery;
+	this->acquisition_cost = this->total_battery;
 }
 
 bool EAV::operator==(const EAV& a) const {
--- EADARP/EAV.h
+++ EADARP/EAV.h
@@ -3,13 +3,14 @@ class EAV {
 public:
 	int id;
 	int capacity;
-	double battery;
+	double total_battery;
 	double start_time;
 	double end_time;
 	double battery_return_percentage;
+	double initial_battery;
 	double acquisition_cost;
 	EAV() {};
-	EAV(int id, int capacity, double battery, double start_time, double end_time, double battery_return_percentage);
+	EAV(int id, int capacity, double battery, double start_time, double end_time, double initial_battery, double battery_return_percentage);
 	~EAV() {};
 	bool operator==(const EAV& a) const;
 	bool operator<(const EAV& a) const;
